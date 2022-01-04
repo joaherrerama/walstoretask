5593d56806350c935ad72cc59772f1bdbc6c6176
Jorge Andres Herrera Maldonado <herreram.jahm@gmail.com>
2022-01-04 22:09:03 +0100

    black process - style enhancement

--- walStoreTask/wsgi.py
+++ walStoreTask/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'walStoreTask.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "walStoreTask.settings")
 
 application = get_wsgi_application()
